f9cc071f1e0b9085b3f7200eaac00bf15150f5de
E.Keerthi Aishwarya <aishukeerthi@users.noreply.github.com>
2022-07-05 22:32:38 -0500

    Initial commit

---
+++ src/test/java/com/App/demo/MyWebAppApplicationTests.java
@@ -0,0 +1,9 @@
+package com.App.demo;
+
+import org.springframework.boot.SpringApplication;
+public class MyWebAppApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppApplication.class, args);
+	}
+}
